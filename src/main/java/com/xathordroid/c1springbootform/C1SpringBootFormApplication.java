67e6a6161f65bad32cd5bea437dcd7b30bdd5ec7
Xathor <andymarcel.97@gmail.com>
2021-04-04 21:22:21 +0200

    Initial commit.

---
+++ src/main/java/com/xathordroid/c1springbootform/C1SpringBootFormApplication.java
@@ -0,0 +1,13 @@
+package com.xathordroid.c1springbootform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C1SpringBootFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C1SpringBootFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xathordroid/c1springbootform/C1SpringBootFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xathordroid.c1springbootform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C1SpringBootFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
